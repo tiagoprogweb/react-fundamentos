4045f944134099840ac5a81c6d9013c1f267c791
tiago-progwebt23 <tiago.progwebt23b@gmail.com>
2023-12-06 17:53:28 -0300

    Config do <a> no Menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,6 +1,16 @@
 /* Menu.jsx */
 import styled from "styled-components";
 
+function Menu() {
+  return (
+    <StyledMenu>
+      <a href="/">Home</a>
+      <a href="/produtos">Produtos</a>
+      <a href="/contato">Contato</a>
+    </StyledMenu>
+  );
+}
+
 const StyledMenu = styled.nav`
   height: 50px;
   display: flex;
@@ -26,15 +36,5 @@ const StyledMenu = styled.nav`
     }
   }
 `;
-
-function Menu() {
-  return (
-    <StyledMenu>
-      <a href="">Home</a>
-      <a href="">Produtos</a>
-      <a href="">Contato</a>
-    </StyledMenu>
-  );
-}
 
 export default Menu;
