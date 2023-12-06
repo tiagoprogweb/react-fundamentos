e5e1625885f9fe9d661edf27c014079c25a28188
tiago-progwebt23 <tiago.progwebt23b@gmail.com>
2023-12-06 17:41:16 -0300

    Contato e Produtos

--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -2,6 +2,19 @@ function Contato() {
   return (
     <section>
       <h2>Contato</h2>
+      <p>Página de fale conosco...</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, ex
+        vitae illo unde ea, quidem aspernatur aliquid voluptas distinctio
+        expedita doloribus architecto praesentium? Dolor suscipit impedit
+        voluptatem maiores, libero nesciunt?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab hic quae
+        architecto doloremque qui mollitia nesciunt, aliquam dicta quo ut nihil
+        dignissimos doloribus excepturi fuga atque laboriosam totam labore
+        dolores.
+      </p>
     </section>
   );
 }
--- src/pages/Produtos.jsx
+++ src/pages/Produtos.jsx
@@ -2,6 +2,13 @@ function Produtos() {
   return (
     <section>
       <h2>Produtos</h2>
+      <p>Página de produtos...</p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit soluta,
+        exercitationem sed asperiores magnam laborum, ipsam maiores nihil,
+        voluptatem fuga reiciendis rem unde dignissimos nesciunt quia recusandae
+        modi quos aspernatur.
+      </p>
     </section>
   );
 }
